14758fc70d41287f04963872dcd518bdd7a59c20
Young-Ho Kim <yghokim@younghokim.net>
2025-12-11 17:27:08 +0900

    Revised the design of profile liniks; used Google Scholar icon button.

--- src/app/_components/ProfileLinksPanel.tsx
+++ src/app/_components/ProfileLinksPanel.tsx
@@ -5,15 +5,18 @@ import Image from 'next/image';
 
 const links = [
     {
-        title: "Google Scholar",
+        title: <Image className='group-hover:opacity-70' src="/assets/googlescholar-icon.png" alt="Google Scholar" width={19} height={19} />,
+        alt: 'Google Scholar',
         url: "https://scholar.google.com/citations?user=UMOierIAAAAJ"
     },
     {
         title: <Image className='group-hover:opacity-70' src="/assets/github-mark.png" alt="Github" width={19} height={19} />,
+        alt: 'Github',
         url: "https://github.com/yghokim"
     },
     {
         title: <Image className='group-hover:opacity-70' src="/assets/linkedin.png" alt="LinkedIn" width={18} height={18} />,
+        alt: 'LinkedIn',
         url: "https://www.linkedin.com/in/younghokim-hci/"
     }
 ]
@@ -27,15 +30,11 @@ export const ProfileLinks = (props: {
     files.sort().reverse()
     const cvPath = path.join("files/cv", files[0])
 
-    return <div className={twMerge(props.className, "text-[11pt]")}>
-            <div className='flex flex-row sm:flex-col justify-center sm:justify-normal'>
-                <div className='flex'>
+    return <div className={twMerge(props.className, "text-[11pt] flex items-center justify-evenly sm:justify-normal")}>
                 {
-                    links.map((link, i) => <a className='group first:border-none sm:border-l-[1px] border-gray-400 px-2' key={i.toString()} href={link.url} target="_blank" rel="noreferrer">{link.title}</a>)
+                    links.map((link, i) => <a className='group px-2' key={i.toString()} href={link.url} target="_blank" rel="noreferrer">{link.title}</a>)
                 }
-                </div>
-                <a className="link-cv px-2 border-gray-400 sm:border-none font-semibold" href={cvPath} target="_blank" rel="noreferrer">Curriculum Vitae</a>
-            </div> 
+                <a className="link-cv px-2 font-semibold" href={cvPath} target="_blank" rel="noreferrer">CV</a>
     </div>
 }
 
